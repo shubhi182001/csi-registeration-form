7ebe37f8617dc805d75cd729ec69ef5cf0a1ef97
shubhi182001 <kediashubhi@gmail.com>
2022-03-27 12:00:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Reg from './components/Reg';
+
+
+function App() {
+  return (
+    <>
+    <Reg/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Reg.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Reg() {
+  return (
+    <>
+    
+    
+    
+    </>
+  )
+}
+
+export default Reg
